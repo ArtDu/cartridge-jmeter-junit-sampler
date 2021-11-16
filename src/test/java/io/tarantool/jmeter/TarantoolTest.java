0541174ae64ce2d39c12d844511fcac46830b22b
Artyom Dubinin <rusartdub@gmail.com>
2021-11-16 12:14:47 +0300

    Sampler for tdg

--- src/test/java/io/tarantool/jmeter/TarantoolTest.java
+++ src/test/java/io/tarantool/jmeter/TarantoolTest.java
@@ -15,6 +15,9 @@ import java.util.*;
 
 public class TarantoolTest {
 
+    public static final int PREDECESSORS = 5;
+    public static final int CROSSREFS = 20;
+    public static final int BOUND = 1_000_000;
     TarantoolCredentials config;
     TarantoolClient<TarantoolTuple, TarantoolResult<TarantoolTuple>> client;
     Map<String, String> env;
@@ -24,8 +27,11 @@ public class TarantoolTest {
     String host;
     String port;
 
+    Random randomGenerator;
 
     public void connectToTarantool() {
+        randomGenerator = new Random();
+
         env = System.getenv();
         user = env.getOrDefault("TNT_USER", "admin");
         password = env.getOrDefault("PASSWORD", "secret-cluster-cookie");
@@ -48,6 +54,116 @@ public class TarantoolTest {
         try {
             List<?> res = client.eval("return 1, require'log'.info('Hello from JAVA')").get();
             assertEquals(1, res.get(0));
+        } catch (Exception e) {
+            // checked exceptions
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void put() {
+        try {
+            int index = randomGenerator.nextInt(BOUND);
+
+            List<Object> predecessors = new ArrayList<>();
+            for (int i = 0; i < PREDECESSORS; i++) {
+                predecessors.add(new HashMap<Object, Object>() {{
+                    put("mdmCode", randomGenerator.nextInt(BOUND));
+                }});
+            }
+
+            List<Object> mdmRelWithSources = new ArrayList<>();
+            for (int i = 0; i < CROSSREFS; i++) {
+                String absCode = String.valueOf(randomGenerator.nextInt(BOUND));
+                String externalId = String.valueOf(randomGenerator.nextInt(BOUND));
+                String crossref = absCode + "~~~" + externalId;
+                mdmRelWithSources.add(new HashMap<Object, Object>() {{
+                    put("absCode", absCode);
+                    put("externalId", externalId);
+                    put("crossref", crossref);
+                }});
+            }
+
+            Map<Object, Object> snapshot = new HashMap<Object, Object>() {{
+                put("id", index);
+                put("mdmCode", index);
+                put("predecessors", predecessors);
+                put("mdmRelWithSources", mdmRelWithSources);
+                put("name", String.valueOf(index));
+            }};
+            HashMap<Object, Object> args = new HashMap<Object, Object>() {{
+                put("etalonClientSnapshot", snapshot);
+                put("etalonModificationDateTime", "2019-03-25T05:47:03.000Z");
+                put("crossrefsModificationDateTime", "2019-03-25T05:47:03.000Z");
+            }};
+            List<?> res = client.eval(
+                    "return repository.put('Etalon', ...)",
+                    Collections.singletonList(args)).get();
+            assertEquals(args, ((ArrayList<?>)res.get(0)).get(0));
+        } catch (Exception e) {
+            // checked exceptions
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    // TODO: CHECK id == integer
+    @Test
+    public void findByMdmCode() {
+        try {
+            Integer index = randomGenerator.nextInt(BOUND);
+            List<?> res = client.eval(
+                    "return repository.find('Etalon', {{'mdmCode_index', '==', ...}})",
+                    Collections.singletonList(index)).get();
+            assertEquals(3, res.size());
+        } catch (Exception e) {
+            // checked exceptions
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void findByMdmCodeInPredecessors() {
+        try {
+            Integer index = randomGenerator.nextInt(BOUND);
+            List<?> res = client.eval(
+                    "return repository.find('Etalon', {{'mdmCode_predecessors_index', '==', ...}})",
+                    Collections.singletonList(index)).get();
+            assertEquals(3, res.size());
+        } catch (Exception e) {
+            // checked exceptions
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void findByCrossref() {
+        try {
+            String absCode = String.valueOf(randomGenerator.nextInt(BOUND));
+            String externalId = String.valueOf(randomGenerator.nextInt(BOUND));
+            String crossref = absCode + "~~~" + externalId;
+            List<?> res = client.eval(
+                    "return repository.find('Etalon', {{'crossref_index', '==', ...}})",
+                    Collections.singletonList(crossref)).get();
+            assertEquals(3, res.size());
+        } catch (Exception e) {
+            // checked exceptions
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void findByNameWithoutIndex() {
+        try {
+            String index = String.valueOf(randomGenerator.nextInt(BOUND));
+            List<?> res = client.eval(
+                    "return repository.find('Etalon', {{'etalonClientSnapshot.name', '==', ...}})",
+                    Collections.singletonList(index)).get();
+            assertEquals(3, res.size());
         } catch (Exception e) {
             // checked exceptions
             e.printStackTrace();
